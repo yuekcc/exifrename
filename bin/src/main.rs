0ed2eae4eaf3a266380841238e771419f524619b
yuekcc <yuekcc@qq.com>
2023-01-30 21:23:38 +0800

    refactor

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,9 +1,8 @@
-use std::os::windows::process::CommandExt;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{self, Command};
 use std::{env, vec};
 
-fn find_python(install_dir: &Path) -> String {
+fn find_python(install_dir: &Path) -> PathBuf {
     #[cfg(target_os = "windows")]
     let cmd_names = vec!["runtime/python.exe", "python.exe"];
 
@@ -12,35 +11,27 @@ fn find_python(install_dir: &Path) -> String {
 
     cmd_names
         .iter()
-        .find(|cmd_name: &&&str| install_dir.join(cmd_name).exists())
-        .unwrap_or(&"python")
-        .to_string()
+        .map(|name| install_dir.join(name))
+        .find(|p| p.exists())
+        .unwrap_or(PathBuf::from("python"))
 }
 
-const CREATE_UNICODE_ENVIRONMENT: u32 = 0x00000400;
-const CREATE_NEW_PROCESS_GROUP: u32 = 0x00000200;
-
-fn start_pyapp(pyexec: &str, install_dir: &Path, cli_args: &[String], work_dir: &Path) {
-    #[cfg(target_os = "windows")]
-    {
-        Command::new(pyexec)
-            .creation_flags(CREATE_NEW_PROCESS_GROUP | CREATE_UNICODE_ENVIRONMENT) // windows 平台专用
+fn start_app(python_exe: &Path, work_dir: &Path, install_dir: &Path, cli_args: &[String]) {
+    let mut proc = {
+        Command::new(python_exe)
             .arg(install_dir)
             .args(cli_args)
             .current_dir(work_dir)
             .spawn()
-            .expect("failed to launch python script");
+            .expect("failed to launch python script")
     };
 
-    #[cfg(not(target_os = "windows"))]
-    {
-        Command::new(pyexec)
-            .arg(install_dir)
-            .args(cli_args)
-            .current_dir(work_dir)
-            .spawn()
-            .expect("failed to launch python script");
-    };
+    let exit_code = proc.wait().expect("app isn't running");
+    if exit_code.success() {
+        process::exit(0);
+    } else {
+        process::exit(exit_code.code().unwrap_or(-1));
+    }
 }
 
 fn main() {
@@ -53,8 +44,8 @@ fn main() {
     let starter_path = PathBuf::from(&starter);
     let install_dir = starter_path.parent().unwrap();
 
-    let python_cmd = find_python(install_dir);
-    println!("using python: {}", &python_cmd);
+    let python_exe = find_python(install_dir);
+    println!("using python: {}", python_exe.display());
 
-    start_pyapp(&python_cmd, install_dir, &cli_args, &work_dir);
+    start_app(&python_exe, &work_dir, install_dir, &cli_args);
 }
